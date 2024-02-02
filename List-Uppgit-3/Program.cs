44bae546ef30c2e0c457c237ca24efef6be38927
HerrBergenholtz <zbdv8800@gmail.com>
2024-02-02 13:04:04 +0100

    Add project files.

---
+++ List-Uppgit-3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Uppgift_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Dictionary<string, int> länder = new()
+            {
+                { "sverige", 450 },
+                { "norge", 385 },
+                { "dannmark", 43 },
+                { "finland", 338 }
+            };
+
+                Console.WriteLine("Skriv in ett land.");
+                Console.WriteLine("Sverige, Norge, Dannmark eller Finland");
+
+                string nyckel = Console.ReadLine().ToLower();
+
+                Console.WriteLine(länder[nyckel]);
+            }
+
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("Du skrev inte in ett lämpligt land");
+                Console.ReadKey();
+                Main();
+            }
+            catch
+            {
+                Console.WriteLine("Något gick snett");
+                Console.ReadKey();
+                Main();
+            }
+        }
+    }
+}
